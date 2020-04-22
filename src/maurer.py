0b36f6d23b4158ecf14c1dedae7a4f60e7006d70
ems236 <ems236@case.edu>
2020-04-21 22:30:36 -0400

    more maurer setup

--- src/maurer.py
+++ src/maurer.py
@@ -1,18 +1,20 @@
 import src.random_util
+import secrets
 import math
 
 M_BITS = 20
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]
 
 def trial_prime(bits):
-    while True:
-        p = src.random_util.rand_in_range(1 << bits, 1 << (bits + 1))
+    return PRIMES[src.random_util.rand_in_range(0, len(PRIMES))]
+    """while True:
+        p = src.random_util.rand_in_range(1 << (bits - 1), 1 << bits)
         sqrt = p ** 0.5
         for i in range(0, len(PRIMES)):
             if PRIMES[i] > sqrt:
                 return p
-            else:
-                break
+            elif p == 0 % PRIMES[i]:
+                break"""
 
 def random_prime(bits):
     if bits <= 6:
@@ -26,18 +28,22 @@ def random_prime(bits):
     interval = (1 << (bits - 1)) // (2 * q)
 
     while True:
-        r = src.random_util.rand_in_range(interval + 1, 2 * interval + 1)
-        n = 2 * r * q + 1
+        rand_scale = src.random_util.rand_in_range(interval + 1, 2 * interval + 1)
+        n = 2 * rand_scale * q + 1
 
         #test by pocklington
-        a = src.random_util(2, n-1)
+        a = src.random_util.rand_in_range(2, n-1)
         b = pow(a, n-1, n)
         if b == 1:
-            test = pow(a, 2*r, n) - 1
+            test = pow(a, 2 * rand_scale, n) - 1
             if math.gcd(test, q) == 1:
                 return n    
         
-
-
 def relative_size(bits):
-    pass
+    s = secrets.randbelow((1 << 100) + 1) / (1 << 100)
+    r = 2**(s-1)
+    while bits - bits * r > M_BITS:
+        s = secrets.randbelow((1 << 100) + 1) / (1 << 100)
+        r = 2**(s-1)
+    
+    return r
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import src.miller_rabin
 import src.joye_paillier
+import src.maurer
 import math
 
 src.miller_rabin.miller_rabin_is_prime(6073)
 x = src.joye_paillier.PaillierPrimeGenerator(1024)
 print(x.rand_prime())
 print(math.log2(x.rand_prime()))
+
+for _ in range(0, 10):
+    p = src.maurer.random_prime(10)
+    print(p)
