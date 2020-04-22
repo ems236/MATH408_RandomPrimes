759e7cda2f98bcb8bf3da7d34acdd0a6dee91ccc
ems236 <ems236@case.edu>
2020-04-22 11:28:00 -0400

    test 4096

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import timeit
 with open('timing.csv', 'w', newline='') as results:
     writer = csv.writer(results)
     writer.writerow(["bits", "Miller-Rabin", "Joye-Paillier", "Maurer"])
-    for i in range(4, 12):
+    for i in range(4, 13):
         bits = 1 << i
         miller = timeit.timeit(lambda: src.miller_rabin.rand_prime(bits), number=10)
         paillier_gen = src.joye_paillier.PaillierPrimeGenerator(bits)
